7816fd12ad792b00cb295f4d979ce15532a8f905
FernandoLomazzi <78239004+FernandoLomazzi@users.noreply.github.com>
2024-10-19 18:24:23 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
 window.location = "/profile";
-document.getElementsByTagName('input')[0].value = "hola";
-document.getElementsByTagName('button')[0].click();
+const fileInput = document.getElementById('profile_pic');
+
+async function fetchImageAsFile(url, fileName) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return new File([blob], fileName, { type: blob.type });
+}
+
+fetchImageAsFile('https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg', 'image.jpg')
+    .then((myFile) => {
+        const dataTransfer = new DataTransfer();
+        dataTransfer.items.add(myFile);
+        fileInput.files = dataTransfer.files;
+
+        // Submit the form after setting the file
+        document.getElementsByTagName('form')[0].submit();
+    })
+    .catch((error) => {
+        console.error('Error fetching image:', error);
+    });
