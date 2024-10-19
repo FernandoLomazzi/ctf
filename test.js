b3ce489878c82865c8c767fc8e50772e6203d066
FernandoLomazzi <78239004+FernandoLomazzi@users.noreply.github.com>
2024-10-19 18:27:03 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 window.location = "/profile";
-const fileInput = document.getElementById('profile_pic');
 
-async function fetchImageAsFile(url, fileName) {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    return new File([blob], fileName, { type: blob.type });
-}
+window.onload = function () {
+    const fileInput = document.getElementById('profile_pic');
 
-fetchImageAsFile('https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg', 'image.jpg')
-    .then((myFile) => {
-        const dataTransfer = new DataTransfer();
-        dataTransfer.items.add(myFile);
-        fileInput.files = dataTransfer.files;
+    async function fetchImageAsFile(url, fileName) {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        return new File([blob], fileName, { type: blob.type });
+    }
 
-        // Submit the form after setting the file
-        document.getElementsByTagName('form')[0].submit();
-    })
-    .catch((error) => {
-        console.error('Error fetching image:', error);
-    });
+    fetchImageAsFile('https://i0.wp.com/puppis.blog/wp-content/uploads/2022/02/abc-cuidado-de-los-gatos-min.jpg', 'image.jpg')
+        .then((myFile) => {
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(myFile);
+            fileInput.files = dataTransfer.files;
+
+            document.getElementsByTagName('form')[0].submit();
+        })
+        .catch((error) => {
+            console.error('Error fetching image:', error);
+        });
+};
